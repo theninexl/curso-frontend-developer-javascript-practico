ceae8c96d90e892f52dea386ef461b900b300de9
theninexl <thenine@thenine.es>
2023-03-01 18:09:55 +0100

    Añadiendo menu mobile

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const hamburguerIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
-menuEmail.addEventListener('click', toggleDesktopMenu);
+menuEmail.addEventListener('click', function(){toggleElement(desktopMenu)});
+hamburguerIcon.addEventListener('click', function(){toggleElement(mobileMenu)});
 
-function toggleDesktopMenu() {
+function toggleElement(elemento) {
+    elemento.classList.toggle('inactive');
+}
+
+/*function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
 }
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
+}*/
